8882705f74617b143e5425fc6f1941eeeb77524c
Danial <danny00b@gmail.com>
2020-09-15 23:20:28 -0700

    Initial website landing page completed

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1fb03004385be1d5e9f9";
+/******/ 	var hotCurrentHash = "d6c8e784b812e41b133c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
