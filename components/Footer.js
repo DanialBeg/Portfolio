aef37ecd22b40ad57258bf362a0f4d6dd1dfe3e0
Danial <danny00b@gmail.com>
2021-07-14 17:18:45 -0700

    Initial header and footers :construction:

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from '../styles/footer.module.css'
+
+export default function Footer() {
+    return(
+        <footer>
+            <div>
+                
+            </div>
+        </footer>
+    )
+}
